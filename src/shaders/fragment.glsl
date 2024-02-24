4a59e34689f143b3e6d484b28a38a22a38dfb55f
GimhanaDissanayake <gimhanadev@gmail.com>
2024-02-24 19:38:21 +0530

    created the first shader

---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,9 @@
+void main() {
+
+    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+
+}
+
+// The fragment shader main function must set the value of gl_FragColor to a rgba format value.
+
+// Each channel of a rgba format color takes a value between 0.0 and 1.0
---
+++ src/shaders/vertex.glsl
@@ -0,0 +1,9 @@
+void main() {
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+}
+
+// A vertex shader must set gl_Position for every vertex in the mesh.
+
+// You need a vertex shader and a fragment shader and each of these must have a main function.
+
+// The vertex shader main function must set the value of gl_Position and it uses the projectionMatrix, the modelViewMatrix and the position of the vertext to do this.
